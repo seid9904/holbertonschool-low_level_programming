f4a8f6cf808e388ad49a2a8aed96bd03113ccb05
gadirov_ <seidgdrv@gmail.com>
2024-10-29 06:52:52 +0000

    task0

---
+++ 0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - n işarəçisinin göstərdiyi dəyəri 98-ə yeniləyir
+ * @n: int tipli işarəçi
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
